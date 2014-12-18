8a82305d027b071be75a72c8457e2ba22dea4c15
Andrew Chin <andrew.chin@squareweave.com.au>
2014-12-19 00:00:47 +1100

    did magic to the header

--- app/assets/javascripts/toys.js
+++ app/assets/javascripts/toys.js
@@ -3,4 +3,17 @@ jQuery(document).ready(function($) {
 
   $(window).stellar();
 
+
+  $(window).on('scroll',function(){
+
+    // we round here to reduce a little workload
+    stop = Math.round($(window).scrollTop());
+    if (stop > 200) {
+      $('.navbar').addClass('navbar-change');
+    } else {
+      $('.navbar').removeClass('navbar-change');
+    }
+
+  });
+
 });
